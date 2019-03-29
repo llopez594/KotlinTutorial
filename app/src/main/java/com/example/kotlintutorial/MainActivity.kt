2d5b62ede510dbd8b96435a9fec2e3658d8d1588
rptos <rptoscoreanos@hotmail.com>
2019-03-29 11:46:31 -0400

    asd

--- app/src/main/java/com/example/kotlintutorial/MainActivity.kt
+++ app/src/main/java/com/example/kotlintutorial/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.kotlintutorial
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mybutton.setOnClickListener {
+            Toast.makeText(this, "hola mundo!", Toast.LENGTH_LONG).show()
+        }
     }
 }
